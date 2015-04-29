c0acfa251945eac94102299e4cbaa125fe09d931
germanodlf <germanodlf@users.noreply.github.com>
2015-04-29 18:00:37 -0300

    Update bacula_zabbix.bash

--- bacula_zabbix.bash
+++ bacula_zabbix.bash
@@ -1,29 +1,51 @@
 #!/bin/bash
 
-baculaClient="$1"
-baculaJobType="$2"
-baculaJobLevel="$3"
-baculaJobExit="$4"
+# Get Bacula's Job ID from parameter
+baculaJobId="$1"
 
-zabbixServ='127.0.0.1'
-zabbixPort='10051'
-zabbixSend='/usr/bin/zabbix_sender'
+# Import configuration file
+source bacula_zabbix.conf
 
-if [ "$baculaJobType" = "Backup" ] ; then
+# 
+case $baculaDbSgdb in
+  P) sql='/usr/bin/psql -U postgres -c' ;;
+  M) sql='/usr/bin/mysql -NBe' ;;
+  *) exit 1 ;;
+esac
 
-  case $baculaJobLevel in
-    "Full")         level='full' ;;
-    "Incremental")  level='incr' ;;
-    "Differential") level='diff' ;;
-    *)              level='erro' ;;
-  esac
+baculaJobType=$($sql "select Type from Job where JobId=$baculaJobId;" $baculaDbName)
+if [ "$baculaJobType" != "B" ] ; then exit 2 ; fi
+  
+baculaJobLevel=$($sql "select Level from Job where JobId=$baculaJobId;" $baculaDbName)
+case $baculaJobLevel in
+  'F') level='full' ;;
+  'D') level='diff' ;;
+  'I') level='incr' ;;
+  *)   exit 3 ;;
+esac
 
-  case $baculaJobExit in
-    "OK")                  code=0 ;;
-    "OK -- with warnings") code=1 ;;
-    *)                     code=2 ;;
-  esac
+baculaJobStatus=$($sql "select JobStatus from Job where JobId=$baculaJobId;" $baculaDbName)
+case $baculaJobStatus in
+  "T") status=0 ;;
+  "W") status=1 ;;
+  *)   status=2 ;;
+esac
 
-  $zabbixSend -z $zabbixServ -p $zabbixPort -s $baculaClient -k "bacula.$level.exit" -o $code
+baculaClientName=$($sql "select Client.Name from Client,Job where Job.ClientId=Client.ClientId and Job.JobId=$baculaJobId;" $baculaDbName)
 
-fi
+$zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.status" -o $status
+  
+baculaJobBytes=$($sql "select JobBytes from Job where JobId=$baculaJobId;" $baculaDbName)
+$zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.bytes" -o $baculaJobBytes
+
+baculaJobFiles=$($sql "select JobFiles from Job where JobId=$baculaJobId;" $baculaDbName)
+$zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.files" -o $baculaJobFiles
+
+baculaJobTime=$($sql "select timestampdiff(second,StartTime,EndTime) from Job where JobId=$baculaJobId;" $baculaDbName)
+$zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.time" -o $baculaJobTime
+
+baculaJobSpeed=$($sql "select round(JobBytes/timestampdiff(second,StartTime,EndTime)/1024,2) from Job where JobId=$baculaJobId;" $baculaDbName)
+$zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.speed" -o $baculaJobSpeed
+
+baculaJobCompr=$($sql "select round(1-JobBytes/ReadBytes,2) from Job where JobId=$baculaJobId;" $baculaDbName)
+$zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.compr" -o $baculaJobCompr
