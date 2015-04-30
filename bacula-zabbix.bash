1d9b16d40cb3e07436f81f145c074ee46bce31ad
germanodlf <germanodlf@users.noreply.github.com>
2015-04-30 14:24:08 -0300

    Update bacula-zabbix.bash

--- bacula-zabbix.bash
+++ bacula-zabbix.bash
@@ -18,11 +18,11 @@ case $baculaDbSgdb in
 esac
 
 # Get Job type from database, then if it is a backup job, proceed, if not, exit
-baculaJobType=$($sql "select Type from Job where JobId=$baculaJobId;")
+baculaJobType=$($sql "select Type from Job where JobId=$baculaJobId;" 2>/dev/null)
 if [ "$baculaJobType" != "B" ] ; then exit 9 ; fi
 
 # Get Job level from database and classify it as Full, Differential, or Incremental
-baculaJobLevel=$($sql "select Level from Job where JobId=$baculaJobId;")
+baculaJobLevel=$($sql "select Level from Job where JobId=$baculaJobId;" 2>/dev/null)
 case $baculaJobLevel in
   'F') level='full' ;;
   'D') level='diff' ;;
@@ -31,7 +31,7 @@ case $baculaJobLevel in
 esac
 
 # Get Job exit status from database and classify it as OK, OK with warnings, or Fail
-baculaJobStatus=$($sql "select JobStatus from Job where JobId=$baculaJobId;")
+baculaJobStatus=$($sql "select JobStatus from Job where JobId=$baculaJobId;" 2>/dev/null)
 if [ -z $baculaJobStatus ] ; then exit 13 ; fi
 case $baculaJobStatus in
   "T") status=0 ;;
@@ -40,7 +40,7 @@ case $baculaJobStatus in
 esac
 
 # Get client's name from database
-baculaClientName=$($sql "select Client.Name from Client,Job where Job.ClientId=Client.ClientId and Job.JobId=$baculaJobId;")
+baculaClientName=$($sql "select Client.Name from Client,Job where Job.ClientId=Client.ClientId and Job.JobId=$baculaJobId;" 2>/dev/null)
 if [ -z $baculaClientName ] ; then exit 15 ; fi
 
 # Initialize return as zero
@@ -51,27 +51,27 @@ $zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacul
 if [ $? -ne 0 ] ; then return=$(($return+1)) ; fi
 
 # Get from database the number of bytes transferred by the Job and send it to Zabbix server
-baculaJobBytes=$($sql "select JobBytes from Job where JobId=$baculaJobId;")
+baculaJobBytes=$($sql "select JobBytes from Job where JobId=$baculaJobId;" 2>/dev/null)
 $zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.bytes" -o $baculaJobBytes
 if [ $? -ne 0 ] ; then return=$(($return+2)) ; fi
 
 # Get from database the number of files transferred by the Job and send it to Zabbix server
-baculaJobFiles=$($sql "select JobFiles from Job where JobId=$baculaJobId;")
+baculaJobFiles=$($sql "select JobFiles from Job where JobId=$baculaJobId;" 2>/dev/null)
 $zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.files" -o $baculaJobFiles
 if [ $? -ne 0 ] ; then return=$(($return+4)) ; fi
 
 # Get from database the time spent by the Job and send it to Zabbix server
-baculaJobTime=$($sql "select timestampdiff(second,StartTime,EndTime) from Job where JobId=$baculaJobId;")
+baculaJobTime=$($sql "select timestampdiff(second,StartTime,EndTime) from Job where JobId=$baculaJobId;" 2>/dev/null)
 $zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.time" -o $baculaJobTime
 if [ $? -ne 0 ] ; then return=$(($return+8)) ; fi
 
 # Get Job speed from database and send it to Zabbix server
-baculaJobSpeed=$($sql "select round(JobBytes/timestampdiff(second,StartTime,EndTime)/1024,2) from Job where JobId=$baculaJobId;")
+baculaJobSpeed=$($sql "select round(JobBytes/timestampdiff(second,StartTime,EndTime)/1024,2) from Job where JobId=$baculaJobId;" 2>/dev/null)
 $zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.speed" -o $baculaJobSpeed
 if [ $? -ne 0 ] ; then return=$(($return+16)) ; fi
 
 # Get Job compression rate from database and send it to Zabbix server
-baculaJobCompr=$($sql "select round(1-JobBytes/ReadBytes,2) from Job where JobId=$baculaJobId;")
+baculaJobCompr=$($sql "select round(1-JobBytes/ReadBytes,2) from Job where JobId=$baculaJobId;" 2>/dev/null)
 $zabbixSender -z $zabbixSrvAddr -p $zabbixSrvPort -s $baculaClientName -k "bacula.$level.job.compr" -o $baculaJobCompr
 if [ $? -ne 0 ] ; then return=$(($return+32)) ; fi
 
